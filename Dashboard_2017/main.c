2f8680b9108d69e3bcaa445f1bd73b4be026140c
DESKTOP-QP1DVTO\Sondre <sondreixix@gmail.com>
2017-03-18 15:17:35 +0100

    Added Atmel Studio project and UM driver submodule

---
+++ Dashboard_2017/main.c
@@ -0,0 +1,18 @@
+/*
+ * Dashboard_2017.c
+ *
+ * Created: 18/03/17 15:14:29
+ * Author : Sondre
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
