afd12fd3ac7dfcd0d720e51eae5012d45a6d7e0a
unknown <olesot@stud.ntnu.no>
2017-04-21 20:36:50 +0200

    Alternativ vindusvisker, ikke testet

--- Dashboard_2017/windowwiper.c
+++ Dashboard_2017/windowwiper.c
@@ -20,6 +20,7 @@ uint16_t ww_angle = WW_BOTTOM_ANGLE;
 uint16_t ww_adc_val = 0;
 uint8_t ww_dir = 0;
 uint8_t ww_delay = 0;
+uint8_t ww_speed = 0;
 
 void window_wiper_init()
 {
@@ -35,11 +36,22 @@ void window_wiper_init()
 void window_wiper()
 {
 	ww_adc_val = adc_read(CH_ADC0);
-	
+	/*
 	if (ww_count < ww_adc_val)
 	{
 		ww_count++;
 		return;
+	}
+	*/
+	
+	if (ww_adc_val > 1000){
+		ww_speed = 0;
+	}else if(ww_adc_val > 700){
+		ww_speed = 1;
+	}else if(ww_adc_val > 300){
+		ww_speed = 2;
+	}else{
+		ww_speed = 3;
 	}
 	
 	if (ww_adc_val > 1000)
@@ -67,15 +79,15 @@ void window_wiper()
 		
 		if (ww_dir == 0)
 		{
-			ww_angle = ww_angle + 10;
+			ww_angle = ww_angle + ww_speed;
 		}else if (ww_dir == 1)
 		{
-			ww_angle = ww_angle - 10;
+			ww_angle = ww_angle - ww_speed;
 		}
 		
 		OCR3B = ww_angle;
 	}
-	ww_count = 0;
+	//ww_count = 0;
 }
 
 
